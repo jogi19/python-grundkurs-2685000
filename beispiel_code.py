a9c746d1fd15968ab215838fa5d500ea5e3a2059
Jochen Weiss <36038341+jogi19@users.noreply.github.com>
2025-03-21 12:31:20 +0000

    add beispiel_code.py

---
+++ beispiel_code.py
@@ -0,0 +1,18 @@
+#! /usb/bin/python
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Beispiel für argparse")
+parser.add_argument("pflicht_int", help="Integer der erforderlich ist", type=int)
+parser.add_argument("--optional_int", help="Optionaler Integer", type=int)
+parser.add_argument("--optional_str", help="Optionaler String", type=str)
+parser.add_argument("--wahl", help="Meine Wahl", choices=["a", "b", "c", "d"])
+
+args = parser.parse_args()
+
+print("Integer:",args.pflicht_int, "- Type", type(args.pflicht_int))
+print("Optional Integer:",args.optional_int, "- Type", type(args.optional_int))
+print("Optional String:",args.optional_str, "- Type", type(args.optional_str))
+print("Wahl: ", args.wahl)
+
+print('ende')
