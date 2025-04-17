a3d2cee572834c1955fd30bbb4294ef74d875471
Jochen Weiss <jochen.weiss@mail.de>
2025-04-17 07:59:28 +0000

    meine loesung

--- script.py
+++ script.py
@@ -16,3 +16,20 @@ import argparse
 #    - Bei der Division soll eine Fehlerbehandlung implementiert werden, um eine Division durch Null zu vermeiden.
 
 # 4. Geben Sie das Ergebnis der Berechnung aus.
+
+parser = argparse.ArgumentParser(description="Gib zwei Zahlen ein und optional den Operator. Default ist die Addition")
+parser.add_argument("zahl1",help="Zahl1. Diese Zahl ist ein Integer", type=int)
+parser.add_argument("zahl2",help="Zahl2. Diese Zahl ist ein Integer", type=int)
+parser.add_argument("--operation",default="add", help="Operator, default is add", type=str, choices=["add", "sub", "mul", "div"])
+
+args = parser.parse_args()
+
+if (args.operation == "add"):
+        print("Das Ergebnis von "+str(args.zahl1)+" + "+str(args.zahl2)+" ist " + str((args.zahl1+args.zahl2)))
+elif (args.operation == "sub"):
+        print("Das Ergebnis von "+str(args.zahl1)+" - "+str(args.zahl2)+" ist " + str((args.zahl1-args.zahl2)))
+elif (args.operation == "mul"):
+        print("Das Ergebnis von "+str(args.zahl1)+" * "+str(args.zahl2)+" ist " + str((args.zahl1*args.zahl2)))
+elif (args.operation == "div"):
+        print("Das Ergebnis von "+str(args.zahl1)+" / "+str(args.zahl2)+" ist " + str(float(args.zahl1)/float(args.zahl2)))
+
