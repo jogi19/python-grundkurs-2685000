1f4fb1ab710e00d1c6253c6e8fef9fe60d61262e
Jochen Weiss <jochen.weiss@mail.de>
2025-04-17 09:01:54 +0000

    Meine Implemtierung. Etwas Fehlerhaft, aber egal

--- script.py
+++ script.py
@@ -15,3 +15,27 @@
 # Wörter: 5
 
 # Optional: Erweitern Sie das Skript, um auch die Anzahl der Vokale und Konsonanten zu zählen.
+import argparse
+
+parser = argparse.ArgumentParser(description="Dieses Script zählt die Anzahl der Zeichen in einem Text")
+
+parser.add_argument("text",help="Gib einen Text ein, von dem ich die Zeichen und Wörter zählen soll", type=str)
+parser.add_argument("--woerter", help="wieviele Wörter hat der Text ?", type=int, default=None)
+
+args = parser.parse_args()
+
+text_len = len(args.text)
+print("Zeichen: "+ str(text_len))
+if args.woerter is not None:
+    print("Wörter: " + str(args.woerter))
+
+text_wo_space = args.text.replace(" ","")
+print("ohne Leerzeichen: " + str(len(text_wo_space)))
+
+liste_vokale = ["a", "e", "i", "o", "u"]
+count_vokale = 0
+for vokale in liste_vokale:
+    count_vokale = count_vokale + args.text.count(vokale)
+
+print("vokale: "+ str(count_vokale))
+print("kons: "+ str(len(text_wo_space)-count_vokale))
